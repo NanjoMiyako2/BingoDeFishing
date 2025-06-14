b372155c3faf5ee64d56849cab2d44a0e8e66e99
NanjoMiyako2 <140793758+NanjoMiyako2@users.noreply.github.com>
2025-06-15 00:12:16 +0900

    Add files via upload

--- BingoDeFishing.js
+++ BingoDeFishing.js
@@ -6,6 +6,46 @@ const TAB_IDX_TODAY_RESULT = 4
 const TAB_IDX_SAVE_LOAD =5
 const TAB_IDX_HAVING_ITEM = 6
 
+//一ステップにかかる秒数
+const ONE_STEP_SECOND = 2;
+
+const ROULETTE_SQUARE_NUM = 9;
+
+const TABLE_ROW = 5;
+const TABLE_COL = 5;
+
+const TABLE_VALUE_MITEI = 0;
+const TABLE_VALUE_V1 = 1;
+const TABLE_VALUE_V2 = 2;
+const TABLE_VALUE_V3 = 3;
+const TABLE_VALUE_V4 = 4;
+const TABLE_VALUE_V5 = 5;
+const TABLE_VALUE_C1 = 6;
+const TABLE_VALUE_C2 = 7;
+const TABLE_VALUE_C3 = 8;
+
+const BINGO_IDX_MAX = 25;
+
+const TABLE_VALUE_STR_LIST = ["未定", "エサ1", "エサ2", "エサ3", "エサ4",
+							  "エサ5", "コイン1", "コイン5", "コイン10"]
+
+g_FlipIdx = 0;
+g_TableArray1 = [];
+g_ConfirmedSquare = [ [0, 1, 0, 0, 0],
+					  [0, 0, 3, 1, 0],
+					  [0, 0, 0, 1, 0],
+					  [0, 1, 4, 0, 0],
+					  [0, 0, 1, 2, 1] ]
+
+
+var g_TableArray1 = [];
+for(var i=0; i<TABLE_ROW; i++){
+  g_TableArray1[i] = [];
+  for(var j=0; j<TABLE_COL; j++){
+    g_TableArray1[i][j] = '';
+  }
+}
+
 class User {
 
 	Coin = 100;
@@ -52,6 +92,150 @@ function SaveAsTextFile(text, filename){
   
 }
 
+function StartStepExecuteInAdvanceOneStepTab(){
+	g_StepExecuteFlg = true
+	
+	SEFunc1();
+}
+
+
+var SEFunc1 = function StepExecute(){
+	let str2 = ""
+	if(g_StepExecuteFlg == true){
+		currentTime = new Date()
+		minDiff = (currentTime - g_PrevStepTime) / 1000;
+		g_PassedStep = parseInt(minDiff)
+		setTimeout(SEFunc1, ONE_STEP_SECOND * 1000)
+	}
+	
+	
+	BingoFlipCount = parseInt(g_PassedStep / 25);
+	for(i=0; i<BingoFlipCount; i++){
+		showInitSquare();
+		for(j=0; j<BINGO_IDX_MAX; j++){
+			FlipOneSquare(j);
+		}
+		
+		AddTableBonusOnlyOneCell();
+		JudgetBingoHits();
+		
+	}
+	
+	g_FlipIdx = g_FlipIdx + 1;
+	if(g_FlipIdx >= BINGO_IDX_MAX){
+
+		AddTableBonusOnlyOneCell();
+		JudgeBingoHits();
+
+		g_FlipIdx = 0;
+		showInitSquare();
+	}
+	FlipOneSquare(g_FlipIdx);
+
+	if(g_PassedStep >= 1){
+		g_PrevCountStart = 0;
+		g_PassedStep = 0;
+		g_PrevStepTime = currentTime
+	}
+	
+	
+
+}
+
+
+function AddTableBonusOnlyOneCell(){
+}
+
+function JudgeBingoHits(){
+}
+
+function RemoveRouletteValueBtns(){
+
+	for(i=0; i<ROULETTE_SQUARE_NUM; i++){
+		idStr1 = "RouletteValueBtn" + String((i+1));
+		elem1 = document.getElementById(idStr1);
+		
+		if(elem1 != null){
+			elem1.remove();
+		}
+	}
+	
+	return;
+}
+
+function ResetBingoTable(){
+
+	for(i=0; i<TABLE_ROW; i++){
+		for(j=0; j<TABLE_COL; j++){
+			g_TableArray1[i][j] = TABLE_VALUE_MITEI;
+		}
+	}
+	
+	for(i=0; i<TABLE_ROW; i++){
+		for(j=0; j<TABLE_COL; j++){
+			rVal = getRandom(TABLE_VALUE_V1, TABLE_VALUE_C3);
+			g_TableArray1[i][j] = rVal;
+			
+			if(g_ConfirmedSquare[i][j] != TABLE_VALUE_MITEI){
+				g_TableArray1[i][j] = g_ConfirmedSquare[i][j]
+			}
+		}
+	}
+	
+	return
+}
+
+function FlipOneSquare(sqIdx){
+
+	col1 = getCol(sqIdx);
+	row1 = getRow(sqIdx);
+	
+	
+	elemStr1 = "BingoTableSquare" + String((row1+1)) + "_" + String((col1+1));
+	elem1 = document.getElementById(elemStr1);
+	
+	headStr1 = "";
+	if(g_ConfirmedSquare[row1][col1] != TABLE_VALUE_MITEI){
+		headStr1 = "確定マス<br>"
+	}
+	elem1.innerHTML= "<span>" + headStr1 + TABLE_VALUE_STR_LIST[ g_TableArray1[row1][col1] ] + "</span>"
+	
+	elem1.style.backgroundColor = "#b0c4de"
+	
+	return
+}
+
+function showInitSquare(){
+
+	ResetBingoTable();
+	
+	for(i=0; i<TABLE_ROW; i++){
+		for(j=0; j<TABLE_COL; j++){
+			elemStr1 = "BingoTableSquare" + String((i+1))+ "_" + String((j+1));
+			elem1 = document.getElementById(elemStr1);
+			
+			if(g_ConfirmedSquare[i][j] == TABLE_VALUE_MITEI){
+				elem1.innerHTML= "<span>未定</span>"
+			}else{
+				str1 = "確定マス<br>";
+				str1 += TABLE_VALUE_STR_LIST[ g_ConfirmedSquare[i][j] ]
+				str1 =  "<span>" + str1 + "</span>"
+				elem1.innerHTML = str1;
+			}
+			
+			elem1.style.backgroundColor = "#ffffff"
+			
+		}
+	}	
+}
+
+function getRow(sqIdx){
+	return parseInt(sqIdx / TABLE_COL);
+}
+
+function getCol(sqIdx){
+	return (sqIdx % TABLE_ROW)
+}
 
 function save(){
 
@@ -111,4 +295,17 @@ function getRandom( min, max ) {
     var random = Math.floor( Math.random() * (max + 1 - min) ) + min;
   
     return random;
+}
+
+main();
+
+function main(){
+
+ g_PrevStepTime = new Date()
+
+ showInitSquare()
+ 
+ g_FlipIdx = -1;
+ StartStepExecuteInAdvanceOneStepTab();
+ 
 }
